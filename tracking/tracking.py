50ce50cc32385a3671b3bd7f9d4b5a3d6996182d
Nxze <leonardo.rxsso@gmail.com>
2024-04-22 14:00:02 +0200

    tracking update

--- tracking/tracking.py
+++ tracking/tracking.py
@@ -1,5 +1,6 @@
 import os
 from ultralytics import YOLO
+import pickle
 
 # os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -7,4 +8,16 @@ from ultralytics import YOLO
 model = YOLO('yolov8s.pt')
 
 # Perform Tracking
-results = model.track(source=0, show=True, tracker="bytetrack.yaml")
+results = model.track(source=r'tracking\test.mp4', show=True, tracker="bytetrack.yaml")
+
+for result in results:
+
+    # coords_norm = result.boxes.xywhn[0].tolist()
+    # conf = result.boxes.conf[0].tolist()
+    # class_id = int(result.boxes.cls[0].tolist())
+    print(result.boxes)
+    with open('results_boxes.pkl', 'wb') as f:
+        pickle.dump(results.boxes, f)
+    
+
+    break
