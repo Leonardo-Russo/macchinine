77dfa41d7ebc821025f993dcca2c20527de32440
Matteo Pentassuglia <matteopx@gmail.com>
2024-06-05 19:00:49 +0200

    Remove the execution of the create_scene.py script

--- render.py
+++ render.py
@@ -5,11 +5,7 @@ import os
 blender = r"D:\Programs\Blender 4.0\blender.exe"
 blend_path = r"G:\Il mio Drive\Codes\Python\Macchinine\blender\untitled.blend"
 
-# Create a new Blender project
-create_command = [blender, '-b', '-P', 'blender/create_scene.py']
-create_process = subprocess.run(create_command, capture_output=False, text=True)
-
-# Load the Scene in the Blender Project
+# Load the pre-made .blend file in the Blender Project and read the csv data
 load_command = [blender, '-b', blend_path, '-P', 'blender/load_scene.py']
 load_process = subprocess.run(load_command, capture_output=False, text=True)
 
@@ -18,9 +14,6 @@ render_command = [blender, '-b', blend_path, '-P', 'blender/render_scene.py']
 render_process = subprocess.run(render_command, capture_output=False, text=True)
 
 # Check for errors and print output if necessary
-if create_process.returncode != 0:
-    print("Error creating Blender project:")
-    print(create_process.stderr)
 if load_process.returncode != 0:
     print("Error loading camera into Blender project:")
     print(load_process.stderr)
